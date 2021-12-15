0275019dc1c672819e41e9ea139ddbe4ea3b2aba
savitapal8 <78473714+savitapal8@users.noreply.github.com>
2021-12-15 17:30:27 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -20,8 +20,8 @@ resource "google_container_cluster" "primary" {
   initial_node_count       = 1
 
   private_cluster_config {
-      enable_private_nodes = true
-      enable_private_endpoint = true
+      #enable_private_nodes = true
+      #enable_private_endpoint = true
       master_ipv4_cidr_block = "10.0.1.0/28"
   }
 
